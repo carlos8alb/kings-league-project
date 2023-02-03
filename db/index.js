c0a58a9a55eca7519625465da4b97161b417eba8
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-02-03 15:45:54 -0300

    Add and format prettier and rules in eslint

--- db/index.js
+++ db/index.js
@@ -1,22 +1,19 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable semi */
-import { writeFile, readFile } from 'node:fs/promises';
-import path from 'node:path';
+import { writeFile, readFile } from 'node:fs/promises'
+import path from 'node:path'
 
-const DB_PATH = path.join(process.cwd(), '/db/');
+const DB_PATH = path.join(process.cwd(), '/db/')
 
 async function readDBFile(filename) {
-  return readFile(`${DB_PATH}/${filename}.json`, 'utf-8').then(JSON.parse);
+  return readFile(`${DB_PATH}/${filename}.json`, 'utf-8').then(JSON.parse)
 }
 
-export const TEAMS = await readDBFile('teams');
-export const PRESIDENTS = await readDBFile('presidents');
+export const TEAMS = await readDBFile('teams')
+export const PRESIDENTS = await readDBFile('presidents')
 
 export function writeDBFile(dbName, data) {
   return writeFile(
     `${DB_PATH}/${dbName}.json`,
     JSON.stringify(data, null, 2),
     'utf8'
-  );
+  )
 }
--- scrapping/leaderboard.js
+++ scrapping/leaderboard.js
@@ -1,22 +1,19 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable space-before-function-paren */
-/* eslint-disable semi */
-import * as cheerio from 'cheerio';
-import { writeDBFile, TEAMS, PRESIDENTS } from '../db/index.js';
+import * as cheerio from 'cheerio'
+import { writeDBFile, TEAMS, PRESIDENTS } from '../db/index.js'
 
 const URLS = {
-  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
-};
+  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
 
 async function scrape(url) {
-  const res = await fetch(url);
-  const html = await res.text();
-  return cheerio.load(html);
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
 }
 
 async function getLeaderboard() {
-  const $ = await scrape(URLS.leaderboard);
-  const $rows = $('table tbody tr');
+  const $ = await scrape(URLS.leaderboard)
+  const $rows = $('table tbody tr')
 
   const LEADERBOARD_SELECTORS = {
     team: { selector: '.fs-table-text_3', typeOf: 'string' },
@@ -25,43 +22,43 @@ async function getLeaderboard() {
     scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
     concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
     yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
-    redCards: { selector: '.fs-table-text_9', typeOf: 'number' },
-  };
+    redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
+  }
 
   const getTeamFrom = ({ name }) => {
     const { presidentId, ...restOfTeam } = TEAMS.find(
       (team) => team.name === name
-    );
+    )
     const presisent = PRESIDENTS.find(
       (president) => president.id === presidentId
-    );
-    return { ...restOfTeam, presisent };
-  };
+    )
+    return { ...restOfTeam, presisent }
+  }
 
-  const leaderboarSelectorEntries = Object.entries(LEADERBOARD_SELECTORS);
+  const leaderboarSelectorEntries = Object.entries(LEADERBOARD_SELECTORS)
 
-  const leaderboard = [];
+  const leaderboard = []
   $rows.each((_, el) => {
     const leaderboardEntries = leaderboarSelectorEntries.map(
       ([key, { selector, typeOf }]) => {
-        const cleanedValue = $(el).find(selector).text().trim();
-        const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue;
-        return [key, value];
+        const cleanedValue = $(el).find(selector).text().trim()
+        const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+        return [key, value]
       }
-    );
+    )
     const { team: name, ...leaderboardFromTeam } =
-      Object.fromEntries(leaderboardEntries);
+      Object.fromEntries(leaderboardEntries)
 
-    const team = getTeamFrom({ name });
+    const team = getTeamFrom({ name })
 
     leaderboard.push({
       team,
-      ...leaderboardFromTeam,
-    });
-  });
-  return leaderboard;
+      ...leaderboardFromTeam
+    })
+  })
+  return leaderboard
 }
 
-const leaderboard = await getLeaderboard();
+const leaderboard = await getLeaderboard()
 
-await writeDBFile('leaderboard', leaderboard);
+await writeDBFile('leaderboard', leaderboard)
