e0c7960f90a238d194db93dc159ff4a1b39341ea
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-02-03 15:32:49 -0300

    Add president scrapping

---
+++ scrapping/presidents.js
@@ -0,0 +1,6 @@
+// document.querySelector('div.big-player img').src -- URL
+// document.querySelector('div.big-player .el-title').textContent.trim() -- IKER CASILLAS
+// document.querySelector('div.big-player .id-player').textContent.trim() -- presidente
+
+// 1- Save imgs in static
+// 2- Create in DB json file president
