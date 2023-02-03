2cfbb144cd1872193619089e6b6f879041f5cff2
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-02-03 10:51:21 -0300

    Add new endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono';
-import { serveStatic } from 'hono/serve-static.module'
+import { serveStatic } from 'hono/serve-static.module';
 import leaderboard from '../db/leaderboard.json';
+import teams from '../db/teams.json';
+import presidents from '../db/presidents.json';
 
 const app = new Hono();
 
@@ -10,11 +12,35 @@ app.get('/', (c) => {
       endpoint: '/leaderboard',
       description: 'Returns the leaderboard',
     },
+    {
+      endpoint: '/teams',
+      description: 'Returns the teams',
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns the presidents',
+    },
   ]);
 });
 
 app.get('/leaderboard', (c) => {
   return c.json(leaderboard);
+});
+
+app.get('/teams', (c) => {
+  return c.json(teams);
+});
+
+app.get('/teams/:id', (c) => {
+  const id = c.req.param('id');
+  const teamFound = teams.find((team) => team.id === id);
+  return teamFound
+    ? c.json(teamFound)
+    : c.json({ message: 'Team not found' }, 404);
+});
+
+app.get('/presidents', (c) => {
+  return c.json(presidents);
 });
 
 app.get('/static/*', serveStatic({ root: './' }));
